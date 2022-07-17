69dd998ff3d330399eba13201b25d995cdecd1f2
piskarev06 <piskarev2006@mail.ru>
2022-07-17 17:09:28 +0300

    adaptive pagination

--- src/components/Pagination/Pagination.styled.ts
+++ src/components/Pagination/Pagination.styled.ts
@@ -9,9 +9,15 @@ interface PaginationStyledProps {
 export const PaginationStyled = styled.button<PaginationStyledProps>`
 	padding: 10px 16px;
 
+	display: flex;
+	justify-content: center;
+	align-items: center;
+
 	font-weight: 600;
 	font-size: 22px;
 	line-height: 100%;
+
+	width: 50px;
 
 	background: #ffffff;
 	border: 2px solid ${baseTheme.colors.accent};
--- src/components/PaginationList/PaginationList.styled.ts
+++ src/components/PaginationList/PaginationList.styled.ts
@@ -2,5 +2,9 @@ import styled from 'styled-components'
 
 export const PaginationListStyled = styled.ul`
 	display: flex;
+	flex-wrap: wrap;
+	justify-content: center;
+	align-items: center;
+
 	gap: 15px;
 `
